53b67ea0579efee7564306bb17ab4d2bbca153d2
ghapuchi <>
2022-12-15 16:25:27 -0800

    fixed update script intro sentence.

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -2,7 +2,7 @@ import glob
 
 legend_paths = glob.glob('legends/*.md')
 
-readme_str = '# Legends of Iran\nIn memory of the women, men, and children of Iran who lost their lives to the evil Islamic Republic.'
+readme_str = '# Legends of Iran\nIn memory of the women, men, and children of Iran who lost their lives to the evil Islamic Republic.\n'
 for path in legend_paths:
     with open(path, 'r') as fs:
         readme_str += fs.read()
